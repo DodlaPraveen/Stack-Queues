cec36f7577c3aac9c9a2cf93afd3e20afd5b8898
praveen dodla <dodlapraveen09@gmail.com>
2022-08-12 16:52:33 +0530

    Create a Queue

--- StacksandQueues/StacksandQueues/Program.cs
+++ StacksandQueues/StacksandQueues/Program.cs
@@ -6,16 +6,23 @@ namespace StacksandQueues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To LinkedListStack");
-            LinkedListStack stack = new LinkedListStack();
-            stack.push(70);
-            stack.push(30);
-            stack.push(56);
-            stack.Display();
-            stack.peek();
-            stack.pop();
-            stack.Display();
-            stack.isEmpty();
+            Console.WriteLine("Welcome To LinkedListStackandQueues");
+            //LinkedListStack stack = new LinkedListStack();
+            //stack.push(70);
+            //stack.push(30);
+            //stack.push(56);
+            //stack.Display();
+            //stack.peek();
+            //stack.pop();
+            //stack.Display();
+            //stack.isEmpty();
+            //Console.ReadLine();
+            LinkedListQueue queue = new LinkedListQueue();
+            Console.WriteLine("\nLinkedList_Queue");
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
             Console.ReadLine();
         }
     }
